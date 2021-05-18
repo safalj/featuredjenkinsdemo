265fec634bc6ace6feb2d31c705779bffa71f620
Safal Jaiswal <55182@hexaware.com>
2021-05-18 16:09:51 +0530

    third commit

--- Prog1.java
+++ Prog1.java
@@ -3,7 +3,9 @@ public class Prog1
     public public static void main(String args[]) {
         for (int i=0;i<=10;i++)
         {
+            for (int j=0;j<=10;j++){
             System.out.println(i);
+            }
         }
         
     }
